9855e67b86354165bef0e3d8f671ffe5ba3e7346
nasim <nasim@DESKTOP-B4EEMHL.attlocal.net>
2024-11-09 08:41:08 -0800

    wellcome maven

---
+++ src/test/java/world/helloworld/helloworld.java
@@ -0,0 +1,11 @@
+package world.helloworld;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		System.out.println("wellcome maven");
+		// TODO Auto-generated method stub
+
+	}
+
+}
